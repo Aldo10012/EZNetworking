d493924533ec84cd56c612c3a9cc4acb500f1747
Alberto <aldo10012@gmail.com>
2024-07-14 22:59:44 -0700

    added RequestDecoderTests.swift

---
+++ Tests/EZNetworkingTests/Util/RequestDecoderTests.swift
@@ -0,0 +1,53 @@
+//
+//  RequestDecoderTests.swift
+//  
+//
+//  Created by Alberto Dominguez on 7/14/24.
+//
+
+import XCTest
+@testable import EZNetworking
+
+final class RequestDecoderTests: XCTestCase {
+
+    func testDocoderCanDocodeMockJSONIntoDeocdableObject() throws {
+        let sut = RequestDecoder()
+        do {
+            let person = try sut.decode(Person.self, from: mockPersonJsonData)
+            XCTAssertEqual(person.name, "John")
+            XCTAssertEqual(person.age, 30)
+        } catch {
+            XCTAssert(false)
+        }
+    }
+    
+    func testDocoderCanNotDocodeInvalidMockJSONIntoDeocdableObject() throws {
+        let sut = RequestDecoder()
+        do {
+            let person = try sut.decode(Person.self, from: invalidMockPersonJsonData)
+            XCTAssert(false)
+        } catch let error as NetworkingError {
+            XCTAssertEqual(error, NetworkingError.couldNotParse)
+        }
+    }
+
+}
+
+struct Person: Decodable {
+    var name: String
+    var age: Int
+}
+
+let mockPersonJsonData = """
+{
+    "name": "John",
+    "age": 30
+}
+""".data(using: .utf8)!
+
+let invalidMockPersonJsonData = """
+{
+    "Name": "John",
+    "Age": 30
+}
+""".data(using: .utf8)!
