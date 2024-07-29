4fdf7642b035144afc2a8353774b832034548f9f
Alberto <aldo10012@gmail.com>
2024-07-28 18:19:35 -0700

    check error first

--- Sources/EZNetworking/Validator/URLResponseValidator.swift
+++ Sources/EZNetworking/Validator/URLResponseValidator.swift
@@ -9,50 +9,46 @@ public struct URLResponseValidatorImpl: URLResponseValidator {
     public init() {}
 
     public func validate(data: Data?, urlResponse: URLResponse?, error: Error?) throws -> Data {
+        if let error = error {
+            if let urlError = error as? URLError {
+                throw NetworkingError.urlError(urlError)
+            }
+            throw NetworkingError.requestFailed(error)
+        }
         guard let data else {
             throw NetworkingError.noData
         }
         guard let urlResponse else {
             throw NetworkingError.noResponse
         }
+        guard let httpURLResponse = urlResponse as? HTTPURLResponse else {
+            throw NetworkingError.noHTTPURLResponse
+        }
+        if let httpError = HTTPNetworkingError.fromStatusCode(httpURLResponse.statusCode) {
+            throw NetworkingError.httpError(httpError)
+        }
+        return data
+    }
+    
+    public func validateDownloadTask(url: URL?, urlResponse: URLResponse?, error: Error?) throws -> URL {
         if let error = error {
             if let urlError = error as? URLError {
                 throw NetworkingError.urlError(urlError)
             }
             throw NetworkingError.requestFailed(error)
         }
-        guard let httpURLResponse = urlResponse as? HTTPURLResponse else {
-            throw NetworkingError.noHTTPURLResponse
-        }
-        
-        if let httpError = HTTPNetworkingError.fromStatusCode(httpURLResponse.statusCode) {
-            throw NetworkingError.httpError(httpError)
-        }
-
-        return data
-    }
-    
-    public func validateDownloadTask(url: URL?, urlResponse: URLResponse?, error: Error?) throws -> URL {
         guard let url else {
             throw NetworkingError.noURL
         }
         guard let urlResponse else {
             throw NetworkingError.noResponse
         }
-        if let error = error {
-            if let urlError = error as? URLError {
-                throw NetworkingError.urlError(urlError)
-            }
-            throw NetworkingError.requestFailed(error)
-        }
         guard let httpURLResponse = urlResponse as? HTTPURLResponse else {
             throw NetworkingError.noHTTPURLResponse
         }
-        
         if let httpError = HTTPNetworkingError.fromStatusCode(httpURLResponse.statusCode) {
             throw NetworkingError.httpError(httpError)
         }
-
         return url        
     }
 }
