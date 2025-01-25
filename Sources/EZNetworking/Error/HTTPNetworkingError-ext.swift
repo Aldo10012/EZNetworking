5a58a89222d920851ebd396adcf74b12b16fe051
Alberto <aldo10012@gmail.com>
2025-01-24 22:57:04 -0800

    refactor return switch

--- Sources/EZNetworking/Error/HTTPNetworkingError-ext.swift
+++ Sources/EZNetworking/Error/HTTPNetworkingError-ext.swift
@@ -2,25 +2,22 @@ import Foundation
 
 extension HTTPNetworkingError {
     static func fromStatusCode(_ statusCode: Int) -> HTTPNetworkingError? {
-        switch statusCode {
+        return switch statusCode {
 
         // Successful Responses (200-299)
-        case 200...299: return nil
+        case 200...299: nil
 
         // Redirection Messages (300-399)
-        case 300...399:
-            return HTTPNetworkingError.redirectionMessageError(HTTPNetworkingRedirectionError.fromStatusCode(statusCode))
+        case 300...399: HTTPNetworkingError.redirectionMessageError(HTTPNetworkingRedirectionError.fromStatusCode(statusCode))
 
         // Client Errors (400-499)
-        case 400...499:
-            return HTTPNetworkingError.clientSideError(HTTPNetworkingClientError.fromStatusCode(statusCode))
+        case 400...499: HTTPNetworkingError.clientSideError(HTTPNetworkingClientError.fromStatusCode(statusCode))
 
         // Server Errors (500-599)
-        case 500...599:
-            return HTTPNetworkingError.serverSideError(HTTPNetworkingServerError.fromStatusCode(statusCode))
+        case 500...599: HTTPNetworkingError.serverSideError(HTTPNetworkingServerError.fromStatusCode(statusCode))
 
         // Unknown or Unhandled Status Code
-        default: return .unknown
+        default: .unknown
         }
     }
 }
