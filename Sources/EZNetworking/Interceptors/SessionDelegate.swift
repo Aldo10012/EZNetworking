626d342448181333497d8e8523ac85ed80bcb948
Alberto <aldo10012@gmail.com>
2025-03-08 23:08:04 -0800

    updated import XCTest

--- Sources/EZNetworking/Interceptors/SessionDelegate.swift
+++ Sources/EZNetworking/Interceptors/SessionDelegate.swift
@@ -1,33 +1,15 @@
 import Foundation
 
-open class SessionDelegate: NSObject {
-    weak var cacheInterceptor: CacheInterceptor? = nil
-    weak var authenticationInterceptor: AuthenticationInterceptor? = nil
-    weak var redirectInterceptor: RedirectInterceptor? = nil
-    weak var metricsInterceptor: MetricsInterceptor? = nil
-    weak var taskLifecycleInterceptor: TaskLifecycleInterceptor? = nil
-    weak var dataTaskInterceptor: DataTaskInterceptor? = nil
-    weak var downloadTaskInterceptor: DownloadTaskInterceptor? = nil
-    weak var streamTaskInterceptor: StreamTaskInterceptor? = nil
-    weak var webSocketTaskInterceptor: WebSocketTaskInterceptor? = nil
+public class SessionDelegate: NSObject {
+    public weak var cacheInterceptor: CacheInterceptor? = nil
+    public weak var authenticationInterceptor: AuthenticationInterceptor? = nil
+    public weak var redirectInterceptor: RedirectInterceptor? = nil
+    public weak var metricsInterceptor: MetricsInterceptor? = nil
+    public weak var taskLifecycleInterceptor: TaskLifecycleInterceptor? = nil
+    public weak var dataTaskInterceptor: DataTaskInterceptor? = nil
+    public weak var downloadTaskInterceptor: DownloadTaskInterceptor? = nil
+    public weak var streamTaskInterceptor: StreamTaskInterceptor? = nil
+    public weak var webSocketTaskInterceptor: WebSocketTaskInterceptor? = nil
     
-    public init(cacheInterceptor: CacheInterceptor? = nil,
-                authenticationInterceptor: AuthenticationInterceptor? = nil,
-                redirectInterceptor: RedirectInterceptor? = nil,
-                metricsInterceptor: MetricsInterceptor? = nil,
-                taskLifecycleInterceptor: TaskLifecycleInterceptor? = nil,
-                dataTaskInterceptor: DataTaskInterceptor? = nil,
-                downloadTaskInterceptor: DownloadTaskInterceptor? = nil,
-                streamTaskInterceptor: StreamTaskInterceptor? = nil,
-                webSocketTaskInterceptor: WebSocketTaskInterceptor? = nil) {
-        self.cacheInterceptor = cacheInterceptor
-        self.authenticationInterceptor = authenticationInterceptor
-        self.redirectInterceptor = redirectInterceptor
-        self.metricsInterceptor = metricsInterceptor
-        self.taskLifecycleInterceptor = taskLifecycleInterceptor
-        self.dataTaskInterceptor = dataTaskInterceptor
-        self.downloadTaskInterceptor = downloadTaskInterceptor
-        self.streamTaskInterceptor = streamTaskInterceptor
-        self.webSocketTaskInterceptor = webSocketTaskInterceptor
-    }
+    override public init() {}
 }
