b6825a7c0c4bb24130901e53d881923c8033bae3
Alberto <aldo10012@gmail.com>
2025-03-09 13:12:10 -0700

    clear white space

--- Sources/EZNetworking/Interceptors/Protocols/AuthenticationInterceptor.swift
+++ Sources/EZNetworking/Interceptors/Protocols/AuthenticationInterceptor.swift
@@ -3,6 +3,5 @@ import Foundation
 /// Protocol for intercepting URL authentication challenges
 public protocol AuthenticationInterceptor: AnyObject {
     func urlSession(_ session: URLSession, task: URLSessionTask, didReceive challenge: URLAuthenticationChallenge) async -> (URLSession.AuthChallengeDisposition, URLCredential?)
-
     func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge) async -> (URLSession.AuthChallengeDisposition, URLCredential?)
 }
