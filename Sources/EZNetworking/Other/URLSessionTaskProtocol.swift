47a77af6cc4f7a5338fff888af231887e009dd02
Alberto <aldo10012@gmail.com>
2025-07-05 02:26:38 -0700

    update URLSessionTaskProtocol

--- Sources/EZNetworking/Other/URLSessionTaskProtocol.swift
+++ Sources/EZNetworking/Other/URLSessionTaskProtocol.swift
@@ -1,14 +1,21 @@
 import Foundation
 
 public protocol URLSessionTaskProtocol {
+    // data
     func data(for request: URLRequest, delegate: (URLSessionTaskDelegate)?) async throws -> (Data, URLResponse)
     func data(from url: URL, delegate: (URLSessionTaskDelegate)?) async throws -> (Data, URLResponse)
 
+    // dataTask
     func dataTask(with request: URLRequest, completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
     func dataTask(with url: URL, completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
 
+    // downloadTask
     func downloadTask(with url: URL, completionHandler: @escaping @Sendable (URL?, URLResponse?, Error?) -> Void) -> URLSessionDownloadTask
     func download(from url: URL, delegate: (URLSessionTaskDelegate)?) async throws -> (URL, URLResponse)
+
+    // uploadTask
+    func uploadTask(with request: URLRequest, from bodyData: Data?, completionHandler: @escaping @Sendable (Data?, URLResponse?, (any Error)?) -> Void) -> URLSessionUploadTask
+
 }
 
 extension URLSession: URLSessionTaskProtocol {}
