b2300c3ed6809f8e91a8713127d3d1c17754c9a3
Alberto <aldo10012@gmail.com>
2025-09-12 18:59:44 -0700

    update NetworkingError

--- Sources/EZNetworking/Error/NetworkingError.swift
+++ Sources/EZNetworking/Error/NetworkingError.swift
@@ -1,17 +1,9 @@
 import Foundation
 
 public enum NetworkingError: Error {
-    // Internal errors
-    case internalError(InternalError)               /// any internal error
-
-    // HTTP Status Code errors
-    case information(HTTPInformationalStatus, URLResponseHeaders)       /// 1xx status code errors
-    case redirect(HTTPRedirectionStatus, URLResponseHeaders)            /// 3xx status code errors
-    case httpClientError(HTTPClientErrorStatus, URLResponseHeaders)     /// 4xx status code errors
-    case httpServerError(HTTPServerErrorStatus, URLResponseHeaders)     /// 5xx status code errors
-
-    // URL Errors
-    case urlError(URLError)                         /// any URL error
+    case internalError(InternalError)     /// any internal error
+    case httpError(HTTPError)             /// any HRRP error
+    case urlError(URLError)               /// any URL error
 }
 
 extension NetworkingError: Equatable {
@@ -20,17 +12,8 @@ extension NetworkingError: Equatable {
         case let (.internalError(error1), .internalError(error2)):
             return error1 == error2
         
-        case let (.information(error1, _), .information(error2, _)):
-            return error1 == error2
-            
-        case let (.redirect(error1, _), .redirect(error2, _)):
-            return error1 == error2
-
-        case let (.httpClientError(error1, _), .httpClientError(error2, _)):
-            return error1 == error2
-        
-        case let (.httpServerError(error1, _), .httpServerError(error2, _)):
-            return error1 == error2
+        case let (.httpError(error1), .httpError(error2)):
+            return error1.statusCode == error2.statusCode
 
         case let (.urlError(error), .urlError(error2)):
             return error == error2
