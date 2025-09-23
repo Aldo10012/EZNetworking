f5561fbfd0895408ab80a2b772500771f94866a9
Alberto <aldo10012@gmail.com>
2025-09-22 19:46:03 -0700

    move protocols to their own files

--- Sources/EZNetworking/Util/Downloader/FileDownloader.swift
+++ Sources/EZNetworking/Util/Downloader/FileDownloader.swift
@@ -1,22 +1,6 @@
 import Combine
 import Foundation
 
-public typealias DownloadProgressHandler = (Double) -> Void
-public typealias DownloadCompletionHandler = (Result<URL, NetworkingError>) -> Void
-
-public enum DownloadStreamEvent {
-    case progress(Double)
-    case success(URL)
-    case failure(NetworkingError)
-}
-
-public protocol FileDownloadable {
-    func downloadFile(from serverUrl: URL, progress: DownloadProgressHandler?) async throws -> URL
-    func downloadFileTask(from serverUrl: URL, progress: DownloadProgressHandler?, completion: @escaping(DownloadCompletionHandler)) -> URLSessionDownloadTask
-    func downloadFilePublisher(from serverUrl: URL, progress: DownloadProgressHandler?) -> AnyPublisher<URL, NetworkingError>
-    func downloadFileStream(from serverUrl: URL) -> AsyncStream<DownloadStreamEvent>
-}
-
 public class FileDownloader: FileDownloadable {
     private let urlSession: URLSessionTaskProtocol
     private let validator: ResponseValidator
---
+++ Sources/EZNetworking/Util/Downloader/Protocols/FileDownloadable.swift
@@ -0,0 +1,18 @@
+import Combine
+import Foundation
+
+public typealias DownloadProgressHandler = (Double) -> Void
+public typealias DownloadCompletionHandler = (Result<URL, NetworkingError>) -> Void
+
+public protocol FileDownloadable {
+    func downloadFile(from serverUrl: URL, progress: DownloadProgressHandler?) async throws -> URL
+    func downloadFileTask(from serverUrl: URL, progress: DownloadProgressHandler?, completion: @escaping(DownloadCompletionHandler)) -> URLSessionDownloadTask
+    func downloadFilePublisher(from serverUrl: URL, progress: DownloadProgressHandler?) -> AnyPublisher<URL, NetworkingError>
+    func downloadFileStream(from serverUrl: URL) -> AsyncStream<DownloadStreamEvent>
+}
+
+public enum DownloadStreamEvent {
+    case progress(Double)
+    case success(URL)
+    case failure(NetworkingError)
+}
