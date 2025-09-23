b5915ffffa0deed70b731c866d862e7d376c52d3
Alberto <aldo10012@gmail.com>
2025-09-22 20:01:23 -0700

    moved DefaultDownloadTaskInterceptor to its own file

--- Sources/EZNetworking/Util/Downloader/FileDownloader.swift
+++ Sources/EZNetworking/Util/Downloader/FileDownloader.swift
@@ -144,28 +144,3 @@ public class FileDownloader: FileDownloadable {
         }
     }
 }
-
-// MARK: - DefaultDownloadTaskInterceptor
-
-/// Default implementation of DownloadTaskInterceptor
-private class DefaultDownloadTaskInterceptor: DownloadTaskInterceptor {
-    var progress: (Double) -> Void
-    
-    init(progress: @escaping (Double) -> Void = { _ in }) {
-        self.progress = progress
-    }
-    
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        progress(1.0)
-    }
-    
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-        let currentProgress = Double(totalBytesWritten) / Double(totalBytesExpectedToWrite)
-        progress(currentProgress)
-    }
-    
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didResumeAtOffset fileOffset: Int64, expectedTotalBytes: Int64) {
-        let currentProgress = Double(fileOffset) / Double(expectedTotalBytes)
-        progress(currentProgress)
-    }
-}
