04c4ac0e0786ffd500c9b6872a89180ee24da5cc
Alberto <aldo10012@gmail.com>
2025-10-16 19:26:03 -0700

    seperate protocol

--- Sources/EZNetworking/Util/Uploader/Protocols/FileUploadable.swift
+++ Sources/EZNetworking/Util/Uploader/Protocols/FileUploadable.swift
@@ -8,29 +8,37 @@ public enum MultipartFormPart {
     case data(name: String, filename: String?, mimeType: String?, data: Data)
 }
 
-public protocol FileUploadable {
-    // Async/Await
+public protocol DataUploadable {
     func uploadData(_ data: Data, with request: URLRequest, progress: UploadProgressHandler?) async throws -> Data
-    func uploadFile(at fileURL: URL, with request: URLRequest, progress: UploadProgressHandler?) async throws -> Data
-    func uploadMultipart(_ parts: [MultipartFormPart], boundary: String, with request: URLRequest, progress: UploadProgressHandler?) async throws -> Data
-
-    // Completion Handlers
+    
     @discardableResult
     func uploadDataTask(_ data: Data, with request: URLRequest, progress: UploadProgressHandler?, completion: @escaping(UploadCompletionHandler)) -> URLSessionUploadTask
-    @discardableResult
-    func uploadFileTask(_ fileURL: URL, with request: URLRequest, progress: UploadProgressHandler?, completion: @escaping(UploadCompletionHandler)) -> URLSessionUploadTask
+
+    func uploadDataPublisher(_ data: Data, with request: URLRequest, progress: UploadProgressHandler?) -> AnyPublisher<Data, NetworkingError>
+
+    func uploadDataStream(_ data: Data, with request: URLRequest) -> AsyncStream<UploadStreamEvent>
+}
+
+public protocol MultipartFormUploadable {
+    func uploadMultipart(_ parts: [MultipartFormPart], boundary: String, with request: URLRequest, progress: UploadProgressHandler?) async throws -> Data
+
     @discardableResult
     func uploadMultipartTask(_ parts: [MultipartFormPart], boundary: String, with request: URLRequest, progress: UploadProgressHandler?, completion: @escaping(UploadCompletionHandler)) -> URLSessionUploadTask
 
-    // Combine
-    func uploadDataPublisher(_ data: Data, with request: URLRequest, progress: UploadProgressHandler?) -> AnyPublisher<Data, NetworkingError>
-    func uploadFilePublisher(_ fileURL: URL, with request: URLRequest, progress: UploadProgressHandler?) -> AnyPublisher<Data, NetworkingError>
     func uploadMultipartPublisher(_ parts: [MultipartFormPart], boundary: String, with request: URLRequest, progress: UploadProgressHandler?) -> AnyPublisher<Data, NetworkingError>
 
-    // AsyncStream
-    func uploadDataStream(_ data: Data, with request: URLRequest) -> AsyncStream<UploadStreamEvent>
-    func uploadFileStream(_ fileURL: URL, with request: URLRequest) -> AsyncStream<UploadStreamEvent>
     func uploadMultipartStream(_ parts: [MultipartFormPart], boundary: String, with request: URLRequest) -> AsyncStream<UploadStreamEvent>
+}
+
+public protocol FileUploadable {
+    func uploadFile(at fileURL: URL, with request: URLRequest, progress: UploadProgressHandler?) async throws -> Data
+    
+    @discardableResult
+    func uploadFileTask(_ fileURL: URL, with request: URLRequest, progress: UploadProgressHandler?, completion: @escaping(UploadCompletionHandler)) -> URLSessionUploadTask
+
+    func uploadFilePublisher(_ fileURL: URL, with request: URLRequest, progress: UploadProgressHandler?) -> AnyPublisher<Data, NetworkingError>
+
+    func uploadFileStream(_ fileURL: URL, with request: URLRequest) -> AsyncStream<UploadStreamEvent>
 }
 
 public enum UploadStreamEvent {
