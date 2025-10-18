c6b72fa7fb43dd9a732ecb72b52326f10c187321
Alberto <aldo10012@gmail.com>
2025-10-17 21:34:28 -0700

    add FileUploadable protocol

--- Sources/EZNetworking/Util/Uploader/FileUploader/Protocols/FileUploadable.swift
+++ Sources/EZNetworking/Util/Uploader/FileUploader/Protocols/FileUploadable.swift
@@ -1,7 +1,9 @@
-//
-//  FileUploadable.swift
-//  EZNetworking
-//
-//  Created by Alberto Dominguez on 10/17/25.
-//
+import Foundation
+import Combine
 
+public protocol FileUploadable {
+    func uploadFile(at fileURL: URL, with request: Request, progress: UploadProgressHandler?) async throws -> Data
+    func uploadFileTask(_ fileURL: URL, with request: Request, progress: UploadProgressHandler?, completion: @escaping(UploadCompletionHandler)) -> URLSessionUploadTask
+    func uploadFilePublisher(_ fileURL: URL, with request: Request, progress: UploadProgressHandler?) -> AnyPublisher<Data, NetworkingError>
+    func uploadFileStream(_ fileURL: URL, with request: Request) -> AsyncStream<UploadStreamEvent>
+}
