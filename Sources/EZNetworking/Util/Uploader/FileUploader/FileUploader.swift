15132255ceb7fbd8b22280257610df0380549e92
Alberto <aldo10012@gmail.com>
2025-10-17 21:59:57 -0700

    add FileUploader

--- Sources/EZNetworking/Util/Uploader/FileUploader/FileUploader.swift
+++ Sources/EZNetworking/Util/Uploader/FileUploader/FileUploader.swift
@@ -1,7 +1,138 @@
-//
-//  FileUploader.swift
-//  EZNetworking
-//
-//  Created by Alberto Dominguez on 10/17/25.
-//
+import Foundation
+import Combine
 
+public class FileUploader: FileUploadable {
+    private let urlSession: URLSessionTaskProtocol
+    private let validator: ResponseValidator
+    private var sessionDelegate: SessionDelegate
+    
+    private let fallbackUploadTaskInterceptor: DefaultUploadTaskInterceptor = DefaultUploadTaskInterceptor()
+    
+    // MARK: init
+    
+    public init(
+        urlSession: URLSessionTaskProtocol = URLSession.shared,
+        validator: ResponseValidator = ResponseValidatorImpl(),
+        sessionDelegate: SessionDelegate? = nil
+    ) {
+        if let urlSession = urlSession as? URLSession {
+            if let existingDelegate = urlSession.delegate as? SessionDelegate {
+                self.sessionDelegate = existingDelegate
+                self.urlSession = urlSession
+            } else {
+                let newDelegate = sessionDelegate ?? SessionDelegate()
+                let newSession = URLSession(
+                    configuration: urlSession.configuration,
+                    delegate: newDelegate,
+                    delegateQueue: urlSession.delegateQueue
+                )
+                self.sessionDelegate = newDelegate
+                self.urlSession = newSession
+            }
+        } else {
+            self.sessionDelegate = sessionDelegate ?? SessionDelegate()
+            self.urlSession = urlSession
+        }
+        self.validator = validator
+    }
+    
+    // MARK: Async Await
+    
+    public func uploadFile(at fileURL: URL, with request: any Request, progress: UploadProgressHandler?) async throws -> Data {
+        try await withCheckedThrowingContinuation { continuation in
+            self._uploadFileTask(fileURL, with: request, progress: progress) { result in
+                switch result {
+                case .success(let data):
+                    continuation.resume(returning: data)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+        
+    }
+    
+    // MARK: Completion Handler
+    
+    public func uploadFileTask(_ fileURL: URL, with request: any Request, progress: UploadProgressHandler?, completion: @escaping UploadCompletionHandler) -> URLSessionUploadTask? {
+        return _uploadFileTask(fileURL, with: request, progress: progress, completion: completion)
+    }
+    
+    // MARK: Publisher
+    
+    public func uploadFilePublisher(_ fileURL: URL, with request: any Request, progress: UploadProgressHandler?) -> AnyPublisher<Data, NetworkingError> {
+        Future { promise in
+            _ = self._uploadFileTask(fileURL, with: request, progress: progress) { result in
+                promise(result)
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    // MARK: AsyncStream
+    
+    public func uploadFileStream(_ fileURL: URL, with request: any Request) -> AsyncStream<UploadStreamEvent> {
+        AsyncStream { continuation in
+            let progressHandler: UploadProgressHandler = { progress in
+                continuation.yield(.progress(progress))
+            }
+            let task = self._uploadFileTask(fileURL, with: request, progress: progressHandler) { result in
+                switch result {
+                case .success(let data):
+                    continuation.yield(.success(data))
+                case .failure(let error):
+                    continuation.yield(.failure(error))
+                }
+                continuation.finish()
+            }
+            continuation.onTermination = { @Sendable _ in
+                task?.cancel()
+            }
+        }
+    }
+    
+    // MARK: - Core
+    
+    private func _uploadFileTask(_ fileURL: URL, with request: Request, progress: UploadProgressHandler?, completion: @escaping (UploadCompletionHandler)) -> URLSessionUploadTask? {
+        let request = request
+        configureProgressTracking(progress: progress)
+        
+        guard let urlRequest = request.urlRequest else {
+            completion(.failure(.internalError(.noRequest)))
+            return nil
+        }
+        
+        let task = urlSession.uploadTask(with: urlRequest, fromFile: fileURL) { [weak self] data, response, error in
+            guard let self else {
+                completion(.failure(.internalError(.lostReferenceOfSelf)))
+                return
+            }
+            do {
+                try self.validator.validateNoError(error)
+                try self.validator.validateStatus(from: response)
+                let validData = try self.validator.validateData(data)
+                completion(.success(validData))
+            } catch {
+                completion(.failure(self.mapError(error)))
+            }
+        }
+        task.resume()
+        return task
+    }
+    
+    private func mapError(_ error: Error) -> NetworkingError {
+        if let networkError = error as? NetworkingError { return networkError }
+        if let urlError = error as? URLError { return .urlError(urlError) }
+        return .internalError(.unknown)
+    }
+    
+    private func configureProgressTracking(progress: UploadProgressHandler?) {
+        guard let progress else { return }
+        if sessionDelegate.uploadTaskInterceptor != nil {
+            sessionDelegate.uploadTaskInterceptor?.progress = progress
+        } else {
+            fallbackUploadTaskInterceptor.progress = progress
+            sessionDelegate.uploadTaskInterceptor = fallbackUploadTaskInterceptor
+        }
+    }
+}
--- Sources/EZNetworking/Util/Uploader/FileUploader/Protocols/FileUploadable.swift
+++ Sources/EZNetworking/Util/Uploader/FileUploader/Protocols/FileUploadable.swift
@@ -3,7 +3,7 @@ import Combine
 
 public protocol FileUploadable {
     func uploadFile(at fileURL: URL, with request: Request, progress: UploadProgressHandler?) async throws -> Data
-    func uploadFileTask(_ fileURL: URL, with request: Request, progress: UploadProgressHandler?, completion: @escaping(UploadCompletionHandler)) -> URLSessionUploadTask
+    func uploadFileTask(_ fileURL: URL, with request: Request, progress: UploadProgressHandler?, completion: @escaping(UploadCompletionHandler)) -> URLSessionUploadTask?
     func uploadFilePublisher(_ fileURL: URL, with request: Request, progress: UploadProgressHandler?) -> AnyPublisher<Data, NetworkingError>
     func uploadFileStream(_ fileURL: URL, with request: Request) -> AsyncStream<UploadStreamEvent>
 }
