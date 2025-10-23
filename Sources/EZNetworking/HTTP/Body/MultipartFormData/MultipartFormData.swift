16845eac36c0873aa739236ccfba122b0990ae7b
Alberto <aldo10012@gmail.com>
2025-10-23 14:18:40 -0700

    add init skeleton

--- Sources/EZNetworking/HTTP/Body/MultipartFormData/MultipartFormData.swift
+++ Sources/EZNetworking/HTTP/Body/MultipartFormData/MultipartFormData.swift
@@ -2,6 +2,18 @@ import Foundation
 
 public class MultipartFormData: DataConvertible {
     
+    // MARK: Variables
+
+    public let boundary: String
+    
+    // MARK: Init
+    
+    public init(boundary: String) {
+        self.boundary = boundary
+    }
+    
+    // MARK: Data
+    
     public func toData() -> Data? {
         // TODO: implement
         return nil
