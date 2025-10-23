61784098e112618115cf11775d0165d013847f7f
Alberto <aldo10012@gmail.com>
2025-10-23 13:50:52 -0700

    add MultipartFormData - initial

---
+++ Sources/EZNetworking/HTTP/Body/MultipartFormData/MultipartFormData.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public class MultipartFormData: DataConvertible {
+    
+    public func toData() -> Data? {
+        // TODO: implement
+        return nil
+    }
+}
---
+++ Tests/EZNetworkingTests/HTTP/Body/MultipartFormData/MultipartFormDataTests.swift
@@ -0,0 +1,8 @@
+@testable import EZNetworking
+import Foundation
+import Testing
+
+@Suite("Test MultipartFormData")
+class MultipartFormDataTests {
+    // TODO: add tests
+}
