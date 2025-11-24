11b652c99310fb729ab383474d7bc4fe78749f3e
Alberto <aldo10012@gmail.com>
2025-11-24 12:11:54 -0800

    make onEvent optional

--- Sources/EZNetworking/Util/WebSocket/Helpers/DefaultWebSocketTaskInterceptor.swift
+++ Sources/EZNetworking/Util/WebSocket/Helpers/DefaultWebSocketTaskInterceptor.swift
@@ -1,21 +1,21 @@
 import Foundation
 
 internal class DefaultWebSocketTaskInterceptor: WebSocketTaskInterceptor {
-    var onEvent: (WebSocketTaskEvent) -> Void
+    var onEvent: ((WebSocketTaskEvent) -> Void)?
     
-    init(onEvent: @escaping (WebSocketTaskEvent) -> Void = { _ in }) {
+    init(onEvent: ((WebSocketTaskEvent) -> Void)? = nil) {
         self.onEvent = onEvent
     }
     
     func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
-        onEvent(.didOpenWithProtocol(protocolStr: `protocol`))
+        onEvent?(.didOpenWithProtocol(protocolStr: `protocol`))
     }
     
     func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: any Error) {
-        onEvent(.didOpenWithError(error: error))
+        onEvent?(.didOpenWithError(error: error))
     }
     
     func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
-        onEvent(.didClose(code: closeCode, reason: reason))
+        onEvent?(.didClose(code: closeCode, reason: reason))
     }
 }
