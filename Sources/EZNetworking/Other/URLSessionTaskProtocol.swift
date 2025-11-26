4fbb768bf67acbd02d99cfa0a3b3438f1949c756
Alberto <aldo10012@gmail.com>
2025-11-25 18:54:59 -0800

    add WebSocketTaskProtocol

--- Sources/EZNetworking/Other/URLSessionTaskProtocol.swift
+++ Sources/EZNetworking/Other/URLSessionTaskProtocol.swift
@@ -10,7 +10,27 @@ public protocol URLSessionTaskProtocol {
 
     func uploadTask(with request: URLRequest, fromFile fileURL: URL, completionHandler: @escaping @Sendable (Data?, URLResponse?, (any Error)?) -> Void) -> URLSessionUploadTask
 
-    func webSocketTask(with url: URL, protocols: [String]) -> URLSessionWebSocketTask
+    func webSocketTaskInspectable(with url: URL, protocols: [String]) -> WebSocketTaskProtocol // URLSessionWebSocketTask protocol
 }
 
-extension URLSession: URLSessionTaskProtocol {}
+extension URLSession: URLSessionTaskProtocol {
+    public func webSocketTaskInspectable(with url: URL, protocols: [String]) -> WebSocketTaskProtocol {
+        let task: URLSessionWebSocketTask = self.webSocketTask(with: url, protocols: protocols)
+        return task as WebSocketTaskProtocol
+    }
+}
+
+// protocol to allow unit testing and mocking for URLSessionWebSocketTask
+public protocol WebSocketTaskProtocol {
+    func resume()
+    func cancel(with closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?)
+    func sendPing(pongReceiveHandler: @escaping @Sendable ((any Error)?) -> Void)
+    
+    func send(_ message: URLSessionWebSocketTask.Message) async throws
+    func send(_ message: URLSessionWebSocketTask.Message, completionHandler: @escaping @Sendable (Error?) -> Void)
+    
+    func receive() async throws -> URLSessionWebSocketTask.Message
+    func receive(completionHandler: @escaping @Sendable (Result<URLSessionWebSocketTask.Message, Error>) -> Void)
+}
+
+extension URLSessionWebSocketTask: WebSocketTaskProtocol {}
