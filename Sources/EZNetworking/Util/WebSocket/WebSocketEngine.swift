4e3ed342abf283e0f635bbb1cf4d3b3c67c1af8a
Alberto <aldo10012@gmail.com>
2025-11-25 23:54:33 -0800

    set up init

--- Sources/EZNetworking/Util/WebSocket/WebSocketEngine.swift
+++ Sources/EZNetworking/Util/WebSocket/WebSocketEngine.swift
@@ -2,4 +2,32 @@ import Foundation
 
 public actor WebSocketEngine: WebSocketClient {
     
+    private let urlSession: URLSessionTaskProtocol
+    private let sessionDelegate: SessionDelegate?
+    
+    // MARK: - init
+    
+    public init(
+        urlSession: URLSessionTaskProtocol = URLSession.shared,
+        sessionDelegate: SessionDelegate? = nil
+    ) {
+        if let urlSession = urlSession as? URLSession {
+            if let existingDelegate = urlSession.delegate as? SessionDelegate {
+                self.sessionDelegate = existingDelegate
+                self.urlSession = urlSession
+            } else {
+                let newDelegate = sessionDelegate ?? SessionDelegate()
+                let newSession = URLSession(
+                    configuration: urlSession.configuration,
+                    delegate: newDelegate,
+                    delegateQueue: urlSession.delegateQueue
+                )
+                self.sessionDelegate = newDelegate
+                self.urlSession = newSession
+            }
+        } else {
+            self.sessionDelegate = sessionDelegate ?? SessionDelegate()
+            self.urlSession = urlSession
+        }
+    }
 }
