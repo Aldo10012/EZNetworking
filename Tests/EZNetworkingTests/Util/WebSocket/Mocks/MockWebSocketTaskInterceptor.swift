77b97260298dd6ae0e5a9d6ae36a1fe4be3b9651
Alberto <aldo10012@gmail.com>
2025-11-26 16:10:40 -0800

    moved MockWebSocketTaskInterceptor to its own file

---
+++ Tests/EZNetworkingTests/Util/WebSocket/Mocks/MockWebSocketTaskInterceptor.swift
@@ -0,0 +1,39 @@
+import Foundation
+import EZNetworking
+
+let webSocketUrl = URL(string: "ws://127.0.0.1:8080/example")!
+
+private class MockWebSocketTaskInterceptor: WebSocketTaskInterceptor {
+    private let session = URLSession.shared
+    private lazy var task: URLSessionWebSocketTask = {
+        session.webSocketTask(with: webSocketUrl, protocols: [])
+    }()
+    
+    var onEvent: ((WebSocketTaskEvent) -> Void)?
+
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
+        onEvent?(.didOpenWithProtocol(protocolStr: `protocol`))
+    }
+
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: any Error) {
+        onEvent?(.didOpenWithError(error: error))
+    }
+
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+        onEvent?(.didClose(code: closeCode, reason: reason))
+    }
+    
+    // simulate methods
+    
+    func simulateOpenWithProtocol(_ proto: String?) {
+        urlSession(session, webSocketTask: task, didOpenWithProtocol: proto)
+    }
+    
+    func simulateDidCompleteWithError(error: any Error) {
+        urlSession(session, task: task, didCompleteWithError: error)
+    }
+    
+    func simulateDidCloseWithCloseCode(didCloseWith: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+        urlSession(session, webSocketTask: task, didCloseWith: didCloseWith, reason: reason)
+    }
+}
