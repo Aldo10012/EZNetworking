7fc1091ac7f268f9655b4e65282b5e9069054bfa
Alberto <aldo10012@gmail.com>
2025-11-27 00:58:21 -0800

    update MockURLSessionWebSocketTask

--- Tests/EZNetworkingTests/Util/WebSocket/Mocks/MockURLSessionWebSocketTask.swift
+++ Tests/EZNetworkingTests/Util/WebSocket/Mocks/MockURLSessionWebSocketTask.swift
@@ -2,19 +2,88 @@ import Foundation
 import EZNetworking
 
 class MockURLSessionWebSocketTask: WebSocketTaskProtocol {
-    init() {}
 
-    func resume() {}
+    init(resumeClosure: @escaping (() -> Void) = {},
+         sendThrowsError: Bool = false,
+         receiveMessage: String = "",
+         receiveThrowsError: Bool = false
+    ) {
+        self.resumeClosure = resumeClosure
+        self.sendThrowsError = sendThrowsError
+        self.receiveMessage = receiveMessage
+        self.receiveThrowsError = receiveThrowsError
+    }
 
-    func cancel(with closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {}
+    // MARK: resume()
 
-    func send(_ message: URLSessionWebSocketTask.Message, completionHandler: @escaping @Sendable ((any Error)?) -> Void) { }
+    var resumeClosure: () -> Void
+    var didCallResume = false
+    func resume() {
+        didCallResume = true
+        resumeClosure()
+    }
 
-    func send(_ message: URLSessionWebSocketTask.Message) async throws {}
+    // MARK: cancel()
 
-    func receive(completionHandler: @escaping @Sendable (Result<URLSessionWebSocketTask.Message, any Error>) -> Void) { }
+    var didCallCancel = false
+    var didCancelWithCloseCode: URLSessionWebSocketTask.CloseCode?
+    var didCancelWithReason: Data?
+    func cancel(with closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+        didCallCancel = true
+        didCancelWithCloseCode = closeCode
+        didCancelWithReason = reason
+    }
 
-    func receive() async throws -> URLSessionWebSocketTask.Message { .string("") }
+    // MARK: send()
 
-    func sendPing(pongReceiveHandler: @escaping @Sendable ((any Error)?) -> Void) { }
+    var sendThrowsError: Bool
+    func send(_ message: URLSessionWebSocketTask.Message, completionHandler: @escaping @Sendable ((any Error)?) -> Void) {
+        if sendThrowsError {
+            completionHandler(MockURLSessionWebSocketTaskError.failedToSendMessage)
+        }
+    }
+
+    func send(_ message: URLSessionWebSocketTask.Message) async throws {
+        if sendThrowsError {
+            throw MockURLSessionWebSocketTaskError.failedToSendMessage
+        }
+    }
+
+    // MARK: receive()
+
+    var receiveMessage: String
+    var receiveThrowsError: Bool
+    func receive(completionHandler: @escaping @Sendable (Result<URLSessionWebSocketTask.Message, any Error>) -> Void) {
+        if receiveThrowsError {
+            completionHandler(.failure(MockURLSessionWebSocketTaskError.failedToReceiveMessage))
+        } else {
+            completionHandler(.success(.string(receiveMessage)))
+        }
+    }
+
+    func receive() async throws -> URLSessionWebSocketTask.Message {
+        if receiveThrowsError {
+            throw MockURLSessionWebSocketTaskError.failedToReceiveMessage
+        }
+        return .string(receiveMessage)
+    }
+
+    // MARK: sendPing
+
+    var shouldFailPing: Bool = false
+    var pingFailureCount: Int = 0
+    func sendPing(pongReceiveHandler: @escaping @Sendable ((any Error)?) -> Void) {
+        if shouldFailPing {
+            pingFailureCount += 1
+            pongReceiveHandler(MockURLSessionWebSocketTaskError.pingError)
+        } else {
+            pongReceiveHandler(nil)
+        }
+    }
+}
+
+enum MockURLSessionWebSocketTaskError: Error {
+    case pingError
+    case failedToSendMessage
+    case failedToReceiveMessage
 }
