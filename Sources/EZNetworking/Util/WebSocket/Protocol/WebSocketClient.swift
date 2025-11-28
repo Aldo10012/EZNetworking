537b9204fc0bab1b94eb44530ac318a7c757574e
Alberto <aldo10012@gmail.com>
2025-11-28 15:17:42 -0800

    set up WebSocketEngine - initial

--- Sources/EZNetworking/Util/WebSocket/Protocol/WebSocketClient.swift
+++ Sources/EZNetworking/Util/WebSocket/Protocol/WebSocketClient.swift
@@ -25,5 +25,5 @@ public protocol WebSocketClient: Sendable {
     func send(_ message: OutboundMessage) async throws
     
     /// Returns a stream of messages received from the WebSocket server.
-    func messages() -> AsyncThrowingStream<InboundMessage, WebSocketError>
+    func messages() -> AsyncThrowingStream<InboundMessage, Error>
 }
---
+++ Sources/EZNetworking/Util/WebSocket/WebSocketEngine.swift
@@ -0,0 +1,97 @@
+import Foundation
+
+public struct PingConfig {
+    let pingInterval: UInt64
+    let maxPingFailures: Int
+    
+    public init(pingInterval: UInt64 = 30, maxPingFailures: Int = 3) {
+        self.pingInterval = pingInterval
+        self.maxPingFailures = maxPingFailures
+    }
+}
+
+public actor WebSocketEngine: WebSocketClient {
+    
+    // MARK: - variables
+    
+    // Dependencies
+    
+    private let urlSession: URLSessionTaskProtocol
+    private var sessionDelegate: SessionDelegate
+    private let urlRequest: URLRequest
+    private let pingConfic: PingConfig
+    
+    // Default web socket interceptor
+    
+    private let fallbackDownloadTaskInterceptor: WebSocketTaskInterceptor = DefaultWebSocketTaskInterceptor()
+    
+    // WebSocket Task
+    
+    private var webSocketTask: WebSocketTaskProtocol?
+    
+    // MARK: - init
+    
+    public init(
+        urlRequest: URLRequest,
+        pingConfic: PingConfig = PingConfig(),
+        urlSession: URLSessionTaskProtocol = URLSession.shared,
+        sessionDelegate: SessionDelegate? = nil
+    ) {
+        self.urlRequest = urlRequest
+        self.pingConfic = pingConfic
+        if let urlSession = urlSession as? URLSession {
+            // If the session already has a delegate, use it (if it's a SessionDelegate)
+            if let existingDelegate = urlSession.delegate as? SessionDelegate {
+                self.sessionDelegate = existingDelegate
+                self.urlSession = urlSession
+            } else {
+                // If no delegate or not a SessionDelegate, create one
+                let newDelegate = sessionDelegate ?? SessionDelegate()
+                let newSession = URLSession(
+                    configuration: urlSession.configuration,
+                    delegate: newDelegate,
+                    delegateQueue: urlSession.delegateQueue
+                )
+                self.sessionDelegate = newDelegate
+                self.urlSession = newSession
+            }
+        } else {
+            // For mocks or custom protocol types
+            self.sessionDelegate = sessionDelegate ?? SessionDelegate()
+            self.urlSession = urlSession
+        }
+    }
+    
+    // MARK: - state change observation
+    
+    public nonisolated var stateChanges: AsyncStream<WebSocketConnectionState> {
+        // TODO: implement
+        AsyncStream<WebSocketConnectionState> { $0.finish() }
+    }
+    
+    // MARK: - connect
+    
+    public func connect() async throws {
+        // TODO: implement
+    }
+    
+    // MARK: - disconnect
+    
+    public func disconnect(closeCode: URLSessionWebSocketTask.CloseCode?, reason: Data?) async {
+        // TODO: implement
+    }
+    
+    // MARK: - send
+    
+    public func send(_ message: OutboundMessage) async throws {
+        // TODO: implement
+    }
+    
+    // MARK: - messages
+    
+    public nonisolated func messages() -> AsyncThrowingStream<InboundMessage, Error> {
+        // TODO: implement
+        AsyncThrowingStream<InboundMessage, Error> { $0.finish() }
+    }
+    
+}
