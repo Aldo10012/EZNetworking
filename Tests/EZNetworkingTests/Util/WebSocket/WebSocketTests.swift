4db477ace98568fbeeba8c8435b4c85f230c8942
Alberto <aldo10012@gmail.com>
2025-12-30 15:41:29 -0800

    added test suites

---
+++ Tests/EZNetworkingTests/Util/WebSocket/WebSocketTests.swift
@@ -0,0 +1,38 @@
+@testable import EZNetworking
+import Foundation
+import Testing
+
+// MARK: .connect()
+
+@Suite("Test WebSocketEngine.connect()")
+final class WebSocketEngineTests_connect {
+    
+}
+
+// MARK: .disconnect()
+
+@Suite("Test WebSocketEngine.disconnect()")
+final class WebSocketEngineTests_disconnect {
+    
+}
+
+// MARK: .send()
+
+@Suite("Test WebSocketEngine.send()")
+final class WebSocketEngineTests_send {
+    
+}
+
+// MARK: .messages()
+
+@Suite("Test WebSocketEngine.messages()")
+final class WebSocketEngineTests_messages {
+    
+}
+
+// MARK: .stateChanges()
+
+@Suite("Test WebSocketEngine.stateChanges()")
+final class WebSocketEngineTests_stateChanges {
+    
+}
