73c771a61bf23b9ef248a74e13f88b6da413af8a
Alberto <aldo10012@gmail.com>
2025-12-30 15:38:54 -0800

    added init

--- Sources/EZNetworking/Util/WebSocket/WebSocket.swift
+++ Sources/EZNetworking/Util/WebSocket/WebSocket.swift
@@ -2,8 +2,39 @@ import Foundation
 
 public actor WebSocket: WebSocketClient {
     
-    public init() {
-        
+    private let urlSession: URLSessionTaskProtocol
+    private var sessionDelegate: SessionDelegate
+    private let webSocketRequest: URLRequest
+    
+    // MARK: Init
+    
+    public init(
+        urlRequest: URLRequest,
+        urlSession: URLSessionTaskProtocol = URLSession.shared,
+        sessionDelegate: SessionDelegate? = nil
+    ) {
+        self.webSocketRequest = urlRequest
+        if let urlSession = urlSession as? URLSession {
+            // If the session already has a delegate, use it (if it's a SessionDelegate)
+            if let existingDelegate = urlSession.delegate as? SessionDelegate {
+                self.sessionDelegate = existingDelegate
+                self.urlSession = urlSession
+            } else {
+                // If no delegate or not a SessionDelegate, create one
+                let newDelegate = sessionDelegate ?? SessionDelegate()
+                let newSession = URLSession(
+                    configuration: urlSession.configuration,
+                    delegate: newDelegate,
+                    delegateQueue: urlSession.delegateQueue
+                )
+                self.sessionDelegate = newDelegate
+                self.urlSession = newSession
+            }
+        } else {
+            // For mocks or custom protocol types
+            self.sessionDelegate = sessionDelegate ?? SessionDelegate()
+            self.urlSession = urlSession
+        }
     }
     
     public nonisolated var stateChanges: AsyncStream<WebSocketConnectionState> {
