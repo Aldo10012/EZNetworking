e592415c4e0d06df2d588eb16e6c17b53cc94ef9
Alberto <aldo10012@gmail.com>
2026-01-04 16:24:23 -0800

    moved PingConfig to its own file + tests

--- Sources/EZNetworking/Util/WebSocket/WebSocket.swift
+++ Sources/EZNetworking/Util/WebSocket/WebSocket.swift
@@ -248,23 +248,3 @@ extension WebSocket {
         return false
     }
 }
-
-// TODO: move to another file
-public struct PingConfig {
-    public let pingInterval: Duration
-    public let maxPingFailures: UInt
-    
-    public init(pingInterval: Duration = Duration.seconds(30), maxPingFailures: UInt = 3) {
-        self.pingInterval = pingInterval
-        
-        if maxPingFailures == 0 {
-            self.maxPingFailures = 1
-        } else {
-            self.maxPingFailures = maxPingFailures
-        }
-    }
-    
-    internal func waitForPingInterval() async {
-        try? await Task.sleep(for: pingInterval)
-    }
-}
