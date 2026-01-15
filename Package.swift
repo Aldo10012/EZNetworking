c15bc088398bb9eeb18774d2dc2e125909d95fc7
Alberto <aldo10012@gmail.com>
2026-01-14 17:28:46 -0800

    added visionOS support + added build-visionOS job

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         .iOS(.v15), // Set the minimum iOS version here
         .macOS(.v10_15),
         .watchOS(.v8),
-        .tvOS(.v15)
+        .tvOS(.v15),
+        .visionOS(.v1)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
