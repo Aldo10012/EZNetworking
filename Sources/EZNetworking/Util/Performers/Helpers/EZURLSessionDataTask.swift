8939a833ed5b5d477624cf5caf27fdb28fc558cf
Alberto <aldo10012@gmail.com>
2026-01-21 14:11:16 -0800

    made EZURLSessionDataTask thread safe

--- Sources/EZNetworking/Util/Performers/Helpers/EZURLSessionDataTask.swift
+++ Sources/EZNetworking/Util/Performers/Helpers/EZURLSessionDataTask.swift
@@ -1,8 +1,9 @@
 import Foundation
 
 public final class EZURLSessionDataTask: URLSessionDataTask, @unchecked Sendable {
+    private let lock = NSLock()
     private var task: Task<Void, Never>?
-    private var isCancelled = false  // ADD THIS
+    private var isCancelled = false
     private let workBlock: @Sendable () async -> Void
 
     init(performOnResume workBlock: @escaping @Sendable () async -> Void) {
@@ -11,11 +12,17 @@ public final class EZURLSessionDataTask: URLSessionDataTask, @unchecked Sendable
     }
 
     public override func resume() {
+        lock.lock()
+        defer { lock.unlock() }
+
         guard task == nil, !isCancelled else { return } // Prevent double-resume
         task = Task(priority: .high) { await workBlock() }
     }
 
     public override func cancel() {
+        lock.lock()
+        defer { lock.unlock() }
+
         isCancelled = true
         task?.cancel()
     }
