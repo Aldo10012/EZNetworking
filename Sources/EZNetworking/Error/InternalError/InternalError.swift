b50710322ca49eebd914ef006f6e85818b5a61fe
Alberto <aldo10012@gmail.com>
2026-02-06 04:01:59 -0800

    merge enum and extension

--- Sources/EZNetworking/Error/InternalError/InternalError.swift
+++ Sources/EZNetworking/Error/InternalError/InternalError.swift
@@ -1,10 +1,8 @@
 import Foundation
 
-public enum InternalError: Error {
+public enum InternalError: Equatable, Sendable {
     case requestFailed(Error)
-}
 
-extension InternalError: Equatable {
     public static func == (lhs: InternalError, rhs: InternalError) -> Bool {
         switch (lhs, rhs) {
         case let (.requestFailed(lhsError), .requestFailed(rhsError)):
