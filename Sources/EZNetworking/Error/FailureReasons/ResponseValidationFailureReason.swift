94060de92eba449bad764f767db2de2a13de0f8c
Alberto <aldo10012@gmail.com>
2026-02-06 03:21:02 -0800

    added ResponseValidationFailureReason.badHTTPResponse

--- Sources/EZNetworking/Error/FailureReasons/ResponseValidationFailureReason.swift
+++ Sources/EZNetworking/Error/FailureReasons/ResponseValidationFailureReason.swift
@@ -2,4 +2,18 @@ import Foundation
 
 public enum ResponseValidationFailureReason: Equatable, Sendable {
     case noHTTPURLResponse
+    case badHTTPResponse(underlying: HTTPError)
+
+    public static func == (lhs: ResponseValidationFailureReason, rhs: ResponseValidationFailureReason) -> Bool {
+        switch (lhs, rhs) {
+        case (.noHTTPURLResponse, .noHTTPURLResponse):
+            true
+
+        case let (.badHTTPResponse(underlying: error1), .badHTTPResponse(underlying: error2)):
+            error1.statusCode == error2.statusCode
+
+        default:
+            false
+        }
+    }
 }
