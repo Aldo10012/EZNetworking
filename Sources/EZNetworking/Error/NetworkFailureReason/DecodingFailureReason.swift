4e45fea9304297e990e006429c19193dd2fba0f4
Alberto <aldo10012@gmail.com>
2026-02-06 15:26:33 -0800

    [ADDED] DecodingFailureReason + tests

---
+++ Sources/EZNetworking/Error/NetworkFailureReason/DecodingFailureReason.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public enum DecodingFailureReason: Equatable, Sendable {
+    case decodingError(underlying: DecodingError)
+    case other(underlying: Error)
+
+    public static func == (lhs: DecodingFailureReason, rhs: DecodingFailureReason) -> Bool {
+        switch (lhs, rhs) {
+        case let (.decodingError(underlying: L), .decodingError(underlying: R)):
+            (L as NSError) == (R as NSError)
+        case let (.other(underlying: L), .other(underlying: R)):
+            (L as NSError) == (R as NSError)
+        default:
+            false
+        }
+    }
+}
