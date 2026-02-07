efe2b3635bdfbc5ec16a11ac5599ad9107eccfab
Alberto <aldo10012@gmail.com>
2026-02-06 16:42:16 -0800

    [ADD] ResponseValidationFailureReason + tests

---
+++ Sources/EZNetworking/Error/NetworkFailureReason/ResponseValidationFailureReason.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+public enum ResponseValidationFailureReason: Equatable, Sendable {
+    case noHTTPURLResponse
+    case badHTTPResponse(underlying: HTTPResponse)
+
+    public static func == (lhs: ResponseValidationFailureReason, rhs: ResponseValidationFailureReason) -> Bool {
+        switch (lhs, rhs) {
+        case (.noHTTPURLResponse, .noHTTPURLResponse):
+            true
+
+        case let (.badHTTPResponse(underlying: error1), .badHTTPResponse(underlying: error2)):
+            error1.statusCode == error2.statusCode
+
+        default:
+            false
+        }
+    }
+}
---
+++ Tests/EZNetworkingTests/Error/NetworkFailureReason/ResponseValidationFailureReasonTests.swift
@@ -0,0 +1,31 @@
+@testable import EZNetworking
+import Foundation
+import Testing
+
+@Suite("Test ResponseValidationFailureReason")
+final class ResponseValidationFailureReasonTests {
+
+    @Test("test ResponseValidationFailureReason is equatable", arguments: zip(list, list))
+    func urlResponseValidationFailureReasoneIsEquatable(inputA: ResponseValidationFailureReason, inputB: ResponseValidationFailureReason) {
+        #expect(inputA == inputB)
+    }
+
+    @Test("test ResponseValidationFailureReason is not equatable to different cases")
+    func urlResponseValidationFailureReasoneIsNotEquatableToDifferentCases() {
+        let reason1 = ResponseValidationFailureReason.noHTTPURLResponse
+        let reason2 = ResponseValidationFailureReason.badHTTPResponse(underlying: HTTPResponse(statusCode: 400))
+        #expect(reason1 != reason2)
+    }
+
+    @Test("test ResponseValidationFailureReason badHTTPResponse not equatable if different")
+    func urlResponseValidationFailureReasoneBadHTTPResponseIsNotEquatableIfDifferent() {
+        let reason1 = ResponseValidationFailureReason.badHTTPResponse(underlying: HTTPResponse(statusCode: 500))
+        let reason2 = ResponseValidationFailureReason.badHTTPResponse(underlying: HTTPResponse(statusCode: 400))
+        #expect(reason1 != reason2)
+    }
+
+    private static let list: [ResponseValidationFailureReason] = [
+        .noHTTPURLResponse,
+        .badHTTPResponse(underlying: HTTPResponse(statusCode: 400))
+    ]
+}
