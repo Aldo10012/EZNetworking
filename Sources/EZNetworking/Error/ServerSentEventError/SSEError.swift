1724a8187a9f06e0fd3f33eae05d9b504516972a
Alberto <aldo10012@gmail.com>
2026-02-09 18:34:34 -0800

    clear white space

--- Sources/EZNetworking/Error/ServerSentEventError/SSEError.swift
+++ Sources/EZNetworking/Error/ServerSentEventError/SSEError.swift
@@ -21,29 +21,22 @@ public enum SSEError: Error, LocalizedError, Sendable {
         switch self {
         case .notConnected:
             "The SSE connection is not currently established."
-
         case .stillConnecting:
             "A connection attempt is already in progress."
-
         case .alreadyConnected:
             "The SSE connection is already established."
-
         case let .connectionFailed(underlying):
             "Failed to establish SSE connection: \(underlying.localizedDescription)"
-
         case .invalidResponse:
             "The server response was not a valid HTTP response."
-
         case let .invalidStatusCode(statusCode):
             "The server returned an invalid status code: \(statusCode). Expected 200 OK."
-
         case let .invalidContentType(contentType):
             if let contentType {
                 "The server returned an invalid Content-Type: '\(contentType)'. Expected 'text/event-stream'."
             } else {
                 "The server did not specify a Content-Type header. Expected 'text/event-stream'."
             }
-
         case .unexpectedDisconnection:
             "The SSE connection was unexpectedly closed."
         }
