dfafe3807fe647063850d902c6cf925bc5d09e2a
Alberto <aldo10012@gmail.com>
2026-02-16 15:02:24 -0800

    update WebSocketCallbackAdapterTests

--- Tests/EZNetworkingTests/Util/WebSocket/Adaptors/WebSocketCallbackAdapterTests.swift
+++ Tests/EZNetworkingTests/Util/WebSocket/Adaptors/WebSocketCallbackAdapterTests.swift
@@ -4,18 +4,180 @@ import Testing
 
 @Suite("Test WebSocketCallbackAdapter")
 final class WebSocketCallbackAdapterTests {
-    // TODO: add tests
+    // MARK: .connect()
+
+    @Test("test WebSocketCallbackAdapter.connect() returns success if actor does not throw")
+    func webSocketCallbackAdapterConnectReturnsSuccessIfActorDoesNotThrow() async throws {
+        let mockWSActor = MockWebSocket()
+        let sut = WebSocketCallbackAdapter(webSocketClient: mockWSActor)
+        let expectation = Expectation()
+
+        var didSucceed = false
+        sut.connect { result in
+            if case .success = result { didSucceed = true }
+            expectation.fulfill()
+        }
+
+        await expectation.fulfillment(within: .milliseconds(100))
+        #expect(didSucceed)
+    }
+
+    @Test("test WebSocketCallbackAdapter.connect() returns failure if actor does throw")
+    func webSocketCallbackAdapterConnectReturnsFailureIfActorDoesThrow() async throws {
+        let mockWSActor = MockWebSocket(connectThrows: DummyError.error)
+        let sut = WebSocketCallbackAdapter(webSocketClient: mockWSActor)
+        let expectation = Expectation()
+
+        var didSucceed = false
+        sut.connect { result in
+            if case .success = result { didSucceed = true }
+            expectation.fulfill()
+        }
+
+        await expectation.fulfillment(within: .milliseconds(100))
+        #expect(!didSucceed)
+    }
+
+    // MARK: .disconnect()
+
+    @Test("test WebSocketCallbackAdapter.disconnect() returns success if actor does not throw")
+    func webSocketCallbackAdapterDisconnectReturnsSuccessIfActorDoesNotThrow() async throws {
+        let mockWSActor = MockWebSocket()
+        let sut = WebSocketCallbackAdapter(webSocketClient: mockWSActor)
+        let expectation = Expectation()
+
+        var didSucceed = false
+        sut.disconnect { result in
+            if case .success = result { didSucceed = true }
+            expectation.fulfill()
+        }
+
+        await expectation.fulfillment(within: .milliseconds(100))
+        #expect(didSucceed)
+    }
+
+    @Test("test WebSocketCallbackAdapter.disconnect() returns failure if actor does throw")
+    func webSocketCallbackAdapterDisconnectReturnsFailureIfActorDoesThrow() async throws {
+        let mockWSActor = MockWebSocket(disconnectThrows: DummyError.error)
+        let sut = WebSocketCallbackAdapter(webSocketClient: mockWSActor)
+        let expectation = Expectation()
+
+        var didSucceed = false
+        sut.disconnect { result in
+            if case .success = result { didSucceed = true }
+            expectation.fulfill()
+        }
+
+        await expectation.fulfillment(within: .milliseconds(100))
+        #expect(!didSucceed)
+    }
+
+    // MARK: .terminate()
+
+    @Test("test WebSocketCallbackAdapter.terminate()")
+    func webSocketCallbackAdapterTerminate() async throws {
+        let mockWSActor = MockWebSocket()
+        let sut = WebSocketCallbackAdapter(webSocketClient: mockWSActor)
+
+        sut.terminate()
+        try? await Task.sleep(for: .milliseconds(100))
+
+        let didTerminate = await mockWSActor.didTerminate
+        #expect(didTerminate)
+    }
+
+    // MARK: .send()
+
+    @Test("test WebSocketCallbackAdapter.send() returns success if actor does not throw")
+    func webSocketCallbackAdapterSendtReturnsSuccessIfActorDoesNotThrow() async throws {
+        let mockWSActor = MockWebSocket()
+        let sut = WebSocketCallbackAdapter(webSocketClient: mockWSActor)
+        let expectation = Expectation()
+
+        var didSucceed = false
+        sut.send(.string("Test")) { result in
+            if case .success = result { didSucceed = true }
+            expectation.fulfill()
+        }
+
+        await expectation.fulfillment(within: .milliseconds(100))
+        #expect(didSucceed)
+    }
+
+    @Test("test WebSocketCallbackAdapter.send() returns failure if actor does not throw")
+    func webSocketCallbackAdapterSendtReturnsFailureIfActorDoesThrow() async throws {
+        let mockWSActor = MockWebSocket(sendThrows: DummyError.error)
+        let sut = WebSocketCallbackAdapter(webSocketClient: mockWSActor)
+        let expectation = Expectation()
+
+        var didSucceed = false
+        sut.send(.string("Test")) { result in
+            if case .success = result { didSucceed = true }
+            expectation.fulfill()
+        }
+
+        await expectation.fulfillment(within: .milliseconds(100))
+        #expect(!didSucceed)
+    }
+
+    // MARK: .messages
+
+    @Test("test WebSocketCallbackAdapter.onMessages()")
+    func webSocketCallbackAdapterOnMessages() async throws {
+        let mockWSActor = MockWebSocket()
+        let sut = WebSocketCallbackAdapter(webSocketClient: mockWSActor)
+
+        var messagesReceived = [InboundMessage]()
+        sut.onMessage { msg in
+            messagesReceived.append(msg)
+        }
+
+        try? await Task.sleep(for: .milliseconds(100))
+        await mockWSActor.messageContinuation.yield(.string("Hello world"))
+        try? await Task.sleep(for: .milliseconds(100))
+
+        #expect(messagesReceived.count == 1)
+    }
+
+    // MARK: .onStateChange
+
+    @Test("test WebSocketCallbackAdapter.onStateChange()")
+    func webSocketCallbackAdapterOnStateChange() async throws {
+        let mockWSActor = MockWebSocket()
+        let sut = WebSocketCallbackAdapter(webSocketClient: mockWSActor)
+
+        var messagesState = [WebSocketConnectionState]()
+        sut.onStateChange { msg in
+            messagesState.append(msg)
+        }
+
+        try? await Task.sleep(for: .milliseconds(100))
+        await mockWSActor.stateContinuation.yield(.connected(protocol: nil))
+        try? await Task.sleep(for: .milliseconds(100))
+
+        #expect(messagesState.count == 1)
+    }
 }
 
+// MARK: Mocks
+
 private actor MockWebSocket: WebSocketClient {
+    // Stream
     var messageContinuation: AsyncStream<InboundMessage>.Continuation
     let _messageStream: AsyncStream<InboundMessage>
-
     var stateContinuation: AsyncStream<WebSocketConnectionState>.Continuation
     let _stateStream: AsyncStream<WebSocketConnectionState>
 
+    init(
+        connectThrows: Error? = nil,
+        disconnectThrows: Error? = nil,
+        sendThrows: Error? = nil
+    ) {
+        self.connectThrows = connectThrows
+        self.disconnectThrows = disconnectThrows
+        self.sendThrows = sendThrows
 
-    init() {
+        // set up streams
         let foo = AsyncStream<InboundMessage>.makeStream()
         messageContinuation = foo.continuation
         _messageStream = foo.stream
@@ -23,23 +185,32 @@ private actor MockWebSocket: WebSocketClient {
         let bar = AsyncStream<WebSocketConnectionState>.makeStream()
         stateContinuation = bar.continuation
         _stateStream = bar.stream
-
     }
 
+    let connectThrows: Error?
     func connect() async throws {
-
+        if let connectThrows {
+            throw connectThrows
+        }
     }
 
+    let disconnectThrows: Error?
     func disconnect() async throws {
-
+        if let disconnectThrows {
+            throw disconnectThrows
+        }
     }
 
+    var didTerminate = false
     func terminate() async {
-
+        didTerminate = true
     }
 
+    var sendThrows: Error?
     func send(_ message: OutboundMessage) async throws {
-
+        if let sendThrows {
+            throw sendThrows
+        }
     }
 
     var messages: AsyncStream<InboundMessage> {
@@ -50,3 +221,5 @@ private actor MockWebSocket: WebSocketClient {
         _stateStream
     }
 }
+
+private enum DummyError: Error { case error }
--- Tests/EZNetworkingTests/Util/WebSocket/Adaptors/WebSocketPublisherAdapterTests.swift
+++ Tests/EZNetworkingTests/Util/WebSocket/Adaptors/WebSocketPublisherAdapterTests.swift
@@ -7,15 +7,25 @@ final class WebSocketPublisherAdapterTests {
     // TODO: add tests
 }
 
+// MARK: Mocks
+
 private actor MockWebSocket: WebSocketClient {
+    // Stream
     var messageContinuation: AsyncStream<InboundMessage>.Continuation
     let _messageStream: AsyncStream<InboundMessage>
-
     var stateContinuation: AsyncStream<WebSocketConnectionState>.Continuation
     let _stateStream: AsyncStream<WebSocketConnectionState>
 
+    init(
+        connectThrows: Error? = nil,
+        disconnectThrows: Error? = nil,
+        sendThrows: Error? = nil
+    ) {
+        self.connectThrows = connectThrows
+        self.disconnectThrows = disconnectThrows
+        self.sendThrows = sendThrows
 
-    init() {
+        // set up streams
         let foo = AsyncStream<InboundMessage>.makeStream()
         messageContinuation = foo.continuation
         _messageStream = foo.stream
@@ -23,25 +33,34 @@ private actor MockWebSocket: WebSocketClient {
         let bar = AsyncStream<WebSocketConnectionState>.makeStream()
         stateContinuation = bar.continuation
         _stateStream = bar.stream
-
     }
 
+    let connectThrows: Error?
     func connect() async throws {
-
+        if let connectThrows {
+            throw connectThrows
+        }
     }
-    
+
+    let disconnectThrows: Error?
     func disconnect() async throws {
-
+        if let disconnectThrows {
+            throw disconnectThrows
+        }
     }
-    
+
+    var didTerminate = false
     func terminate() async {
-
+        didTerminate = true
     }
-    
+
+    var sendThrows: Error?
     func send(_ message: OutboundMessage) async throws {
-
+        if let sendThrows {
+            throw sendThrows
+        }
     }
-    
+
     var messages: AsyncStream<InboundMessage> {
         _messageStream
     }
@@ -50,3 +69,5 @@ private actor MockWebSocket: WebSocketClient {
         _stateStream
     }
 }
+
+private enum DummyError: Error { case error }
