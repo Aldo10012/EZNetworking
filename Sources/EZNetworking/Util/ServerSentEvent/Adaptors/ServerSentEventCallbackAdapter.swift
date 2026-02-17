754c4db2d7fa3f773fcc1e263289466ee2c1a8b9
Alberto <aldo10012@gmail.com>
2026-02-16 16:09:15 -0800

    build concrete implementations

--- Sources/EZNetworking/Util/ServerSentEvent/Adaptors/ServerSentEventCallbackAdapter.swift
+++ Sources/EZNetworking/Util/ServerSentEvent/Adaptors/ServerSentEventCallbackAdapter.swift
@@ -1,23 +1,91 @@
 import Foundation
 
 public final class ServerSentEventCallbackAdapter: ServerSentEventCallbackClient {
-    public func connect(completion: @escaping (Result<Void, any Error>) -> Void) {
-        // TODO: add
+    private let actor: any ServerSentEventClient
+    private var eventHandler: ((ServerSentEvent) -> Void)?
+    private var stateHandler: ((SSEConnectionState) -> Void)?
+    private var eventTask: Task<Void, Never>?
+    private var stateTask: Task<Void, Never>?
+
+    public convenience init(
+        url: String,
+        session: NetworkSession = Session(),
+        retryPolicy: RetryPolicy? = nil,
+        responseValidator: ResponseValidator = DefaultResponseValidator(expectedHttpHeaders: [.contentType(.eventStream)])
+    ) {
+        let request = SSERequest(url: url)
+        self.init(
+            request: request,
+            session: session,
+            retryPolicy: retryPolicy,
+            responseValidator: responseValidator
+        )
     }
-    
-    public func disconnect(completion: @escaping (Result<Void, any Error>) -> Void) {
-        // TODO: add
+
+    public convenience init(
+        request: SSERequest,
+        session: NetworkSession = Session(),
+        retryPolicy: RetryPolicy? = nil,
+        responseValidator: ResponseValidator = DefaultResponseValidator(expectedHttpHeaders: [.contentType(.eventStream)])
+    ) {
+        self.init(
+            serverSentEventClient: ServerSentEventManager(
+                request: request,
+                session: session,
+                retryPolicy: retryPolicy,
+                responseValidator: responseValidator
+            )
+        )
     }
-    
+
+    // For testing only
+    init(serverSentEventClient: any ServerSentEventClient) {
+        actor = serverSentEventClient
+    }
+
+    public func connect(completion: @escaping (Result<Void, Error>) -> Void) {
+        Task {
+            do {
+                try await actor.connect()
+                completion(.success(()))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+
+    public func disconnect(completion: @escaping (Result<Void, Error>) -> Void) {
+        Task {
+            do {
+                try await actor.disconnect()
+                completion(.success(()))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+
     public func terminate() {
-        // TODO: add
+        Task { await actor.terminate() }
     }
-    
+
     public func onEvent(_ handler: @escaping (ServerSentEvent) -> Void) {
-        // TODO: add
+        eventHandler = handler
+        eventTask?.cancel()
+        eventTask = Task {
+            for await event in await actor.events {
+                handler(event)
+            }
+        }
     }
-    
+
     public func onStateChange(_ handler: @escaping (SSEConnectionState) -> Void) {
-        // TODO: add
+        stateHandler = handler
+        stateTask?.cancel()
+        stateTask = Task {
+            for await state in await actor.stateEvents {
+                handler(state)
+            }
+        }
     }
 }
