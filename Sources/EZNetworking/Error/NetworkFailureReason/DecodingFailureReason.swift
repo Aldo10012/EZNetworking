cf4a84c36f05ba839852610de287d5db0acb0546
Alberto <aldo10012@gmail.com>
2026-02-24 01:01:39 -0800

    move SendableError & SendableErrorWrapper to its own file

--- Sources/EZNetworking/Error/NetworkFailureReason/DecodingFailureReason.swift
+++ Sources/EZNetworking/Error/NetworkFailureReason/DecodingFailureReason.swift
@@ -15,28 +15,3 @@ public enum DecodingFailureReason: Equatable, Sendable {
         }
     }
 }
-
-public typealias SendableError = Error & Sendable
-
-/// A thread-safe wrapper for Error types that may not conform to Sendable.
-public struct SendableErrorWrapper: Error, Sendable {
-    public let localizedDescription: String
-    public let domain: String
-    public let code: Int
-
-    public init(_ error: Error) {
-        let nsError = error as NSError
-        self.localizedDescription = nsError.localizedDescription
-        self.domain = nsError.domain
-        self.code = nsError.code
-    }
-}
-
-extension Error {
-    var asSendableError: SendableError {
-        if let sendable = self as? SendableError {
-            return sendable
-        }
-        return SendableErrorWrapper(self)
-    }
-}
