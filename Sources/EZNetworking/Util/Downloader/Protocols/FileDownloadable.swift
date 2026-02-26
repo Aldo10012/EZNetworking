86cc2c97787382b1c4ac20baeee13438e36138c1
Alberto <aldo10012@gmail.com>
2026-02-25 22:06:39 -0800

    Move DownloadEvent Equatable conformance to source file

--- Sources/EZNetworking/Util/Downloader/Protocols/FileDownloadable.swift
+++ Sources/EZNetworking/Util/Downloader/Protocols/FileDownloadable.swift
@@ -14,7 +14,7 @@ public protocol FileDownloadable: Actor {
     func cancel() throws
 }
 
-public enum DownloadEvent: Sendable {
+public enum DownloadEvent: Sendable, Equatable {
     case started
     case progress(Double)
     case paused
@@ -22,4 +22,19 @@ public enum DownloadEvent: Sendable {
     case completed(URL)
     case failed(NetworkingError)
     case cancelled
+
+    public static func == (lhs: DownloadEvent, rhs: DownloadEvent) -> Bool {
+        switch (lhs, rhs) {
+        case (.started, .started), (.paused, .paused), (.resumed, .resumed), (.cancelled, .cancelled):
+            true
+        case let (.progress(lhsP), .progress(rhsP)):
+            lhsP == rhsP
+        case let (.completed(lhsURL), .completed(rhsURL)):
+            lhsURL == rhsURL
+        case let (.failed(lhsE), .failed(rhsE)):
+            lhsE == rhsE
+        default:
+            false
+        }
+    }
 }
--- Tests/EZNetworkingTests/Util/Downloader/FileDownloaderTests.swift
+++ Tests/EZNetworkingTests/Util/Downloader/FileDownloaderTests.swift
@@ -335,28 +335,6 @@ final class FileDownloaderTests {
     }
 }
 
-// MARK: - DownloadEvent Equatable
-
-extension DownloadEvent: Equatable {
-    public static func == (lhs: DownloadEvent, rhs: DownloadEvent) -> Bool {
-        switch (lhs, rhs) {
-        case (.started, .started),
-             (.paused, .paused),
-             (.resumed, .resumed),
-             (.cancelled, .cancelled):
-            true
-        case let (.progress(lhsP), .progress(rhsP)):
-            lhsP == rhsP
-        case let (.completed(lhsURL), .completed(rhsURL)):
-            lhsURL == rhsURL
-        case let (.failed(lhsE), .failed(rhsE)):
-            lhsE == rhsE
-        default:
-            false
-        }
-    }
-}
-
 // MARK: - Mock URLSessionDownloadTask for delegate calls
 
 private class MockDelegateDownloadTask: URLSessionDownloadTask, @unchecked Sendable {
