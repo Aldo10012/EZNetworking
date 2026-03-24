c6ef2e209614071b3ce63641a8e3ebe51fc655d3
Alberto <aldo10012@gmail.com>
2026-03-23 19:09:08 -0700

    update DefaultDownloadTaskInterceptor

--- Sources/EZNetworking/Session/Interceptors/DefaultInterceptors/DefaultDownloadTaskInterceptor.swift
+++ Sources/EZNetworking/Session/Interceptors/DefaultInterceptors/DefaultDownloadTaskInterceptor.swift
@@ -3,23 +3,13 @@ import Foundation
 /// Default implementation of DownloadTaskInterceptor
 class DefaultDownloadTaskInterceptor: DownloadTaskInterceptor {
     var onEvent: (DownloadTaskInterceptorEvent) -> Void
-    private let validator: ResponseValidator
 
-    init(
-        validator: ResponseValidator = DefaultResponseValidator(),
-        onEvent: @escaping (DownloadTaskInterceptorEvent) -> Void = { _ in }
-    ) {
-        self.validator = validator
+    init(onEvent: @escaping (DownloadTaskInterceptorEvent) -> Void = { _ in }) {
         self.onEvent = onEvent
     }
 
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        do {
-            try validator.validateStatus(from: downloadTask.response)
-            onEvent(.onDownloadCompleted(location))
-        } catch {
-            onEvent(.onDownloadFailed(error, resumeData: nil))
-        }
+        onEvent(.onProgress(1.0))
     }
 
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
@@ -32,10 +22,5 @@ class DefaultDownloadTaskInterceptor: DownloadTaskInterceptor {
         guard expectedTotalBytes > 0 else { return }
         let currentProgress = Double(fileOffset) / Double(expectedTotalBytes)
         onEvent(.onProgress(currentProgress))
-    }
-
-    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error) {
-        let resumeData = (error as? URLError)?.downloadTaskResumeData
-        onEvent(.onDownloadFailed(error, resumeData: resumeData))
     }
 }
