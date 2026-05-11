c798be0d39f108ed1eeaf9fb9872ae222919faca
Alberto <aldo10012@gmail.com>
2026-05-10 21:39:49 -0700

    clean up RequestPerformable callback & publisher support

--- Sources/EZNetworking/Services/Performers/Protocols/RequestPerformable.swift
+++ Sources/EZNetworking/Services/Performers/Protocols/RequestPerformable.swift
@@ -6,15 +6,4 @@ public protocol RequestPerformable {
         request: Request,
         decodeTo decodableObject: T.Type
     ) async throws -> T
-
-    func performTask<T: Decodable & Sendable>(
-        request: Request,
-        decodeTo decodableObject: T.Type,
-        completion: @escaping ((Result<T, NetworkingError>) -> Void)
-    ) -> CancellableRequest
-
-    func performPublisher<T: Decodable & Sendable>(
-        request: Request,
-        decodeTo decodableObject: T.Type
-    ) -> AnyPublisher<T, NetworkingError>
 }
